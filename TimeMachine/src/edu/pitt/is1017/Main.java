8c86629cbf70ad0380474e9316437a54274b6372
e <1@!>
2017-03-30 18:03:40 -0400

    testing JSON

--- TimeMachine/src/edu/pitt/is1017/Main.java
+++ TimeMachine/src/edu/pitt/is1017/Main.java
@@ -1,5 +1,17 @@
 package src.edu.pitt.is1017;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import javax.json.JsonException;
+import javax.json.JsonObject;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -15,6 +27,53 @@ public class Main {
 		WeatherServlet.URLConnectionReader(testURL);
 		//WeatherServlet.URLConnectionReader("http://google.com");
 
+		JsonObject json = readJsonFromUrl(testURL);
+		System.out.println(json.toString());
+		System.out.println(json.get("id"));
+
 	}
+
+	private static String readAll(Reader rd)  {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		try {
+			while ((cp = rd.read()) != -1) {
+				sb.append((char) cp);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
+
+	public static JsonObject readJsonFromUrl(String url) {
+		InputStream is = null;
+		try {
+			is = new URL(url).openStream();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			JsonObject json = null;
+			System.out.println("jsonText= " + jsonText);
+			//JsonObject json = new JsonObject();
+			return json;
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
 
 }
