2a0446600cd2b9a23729de17ce9f9140a736b04f
e <1@!>
2017-04-05 20:28:47 -0400

    added response

--- TimeMachine/web/web/LoginServlet.java
+++ TimeMachine/web/web/LoginServlet.java
@@ -19,19 +19,19 @@ import model.BaseFacade;
 @WebServlet({ "/LoginServlet", "/l" })
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	@EJB
 	BaseFacade bf;
-	
-    public LoginServlet() {
-        super();
-    }
-	
-	public static String LoginBean (String u, String p){
-		System.out.println("Login Bean, u="+u+" p="+p);
-		return bf.checkLogin(u, p);
+
+	public LoginServlet() {
+		super();
 	}
-	
+
+//	public static String LoginBean (String u, String p){
+//		System.out.println("Login Bean, u="+u+" p="+p);
+//		return bf.checkLogin(u, p);
+//	}
+
 	public static String LoginBean2 (String u, String p){
 		System.out.println("Login Bean2:  u="+u+" p="+p);
 		return BaseFacade.checkLogin2(u, p);
@@ -43,6 +43,7 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().println("\n\n\nLoginServletTest\n\n\n");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.addHeader("LOGIN_STATUS", "no");
 	}
 
 	/**
@@ -54,78 +55,83 @@ public class LoginServlet extends HttpServlet {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		String u = LoginBean2(username, password);
-		
+
 		if (u != null) {
-		      // Create a session object if it is already not  created.
-		      HttpSession session = request.getSession(true);
-		      // Get session creation time.
-		      Date createTime = new Date(session.getCreationTime());
-		      // Get last access time of this web page.
-		      Date lastAccessTime = new Date(session.getLastAccessedTime());
+			// Create a session object if it is already not  created.
+			HttpSession session = request.getSession(true);
+			// Get session creation time.
+			Date createTime = new Date(session.getCreationTime());
+			// Get last access time of this web page.
+			Date lastAccessTime = new Date(session.getLastAccessedTime());
 
-		      String title = "Welcome Back to my website";
-		      Integer visitCount = new Integer(0);
-		      String visitCountKey = new String("visitCount");
-		      String userIDKey = new String(u);
-		      String userID = new String(u);
+			String title = "Welcome Back to my website";
+			Integer visitCount = new Integer(0);
+			String visitCountKey = new String("visitCount");
+			String userIDKey = new String(u);
+			String userID = new String(u);
 
-		      // Check if this is new comer on your web page.
-		      if (session.isNew()){
-		         title = "Welcome to my website";
-		         session.setAttribute(userIDKey, userID);
-		         log("NewSession//UIDKey:"+userIDKey+" UID:"+userID+" VisitCount:"+visitCount+" createTime:"+ createTime+ " lastAccess:"+lastAccessTime+" Session:"+session.toString());
-		      } else {
-		         visitCount = (Integer)session.getAttribute(visitCountKey);
-		         visitCount = visitCount + 1;
-		         userID = (String)session.getAttribute(userIDKey);
-		         log("OldSession//UIDKey:"+userIDKey+" UID:"+userID+" VisitCount:"+visitCount+" createTime:"+ createTime+ " lastAccess:"+lastAccessTime+" Session:"+session.toString());
-		      }
-		      session.setAttribute(visitCountKey,  visitCount);
-		      
-		      request.setAttribute("user", u);
-		      response.sendRedirect("/TimeMachine/");
+			// Check if this is new comer on your web page.
+			if (session.isNew()){
+				title = "Welcome to my website";
+				session.setAttribute(userIDKey, userID);
+				log("NewSession//UIDKey:"+userIDKey+" UID:"+userID+" VisitCount:"+visitCount+" createTime:"+ createTime+ " lastAccess:"+lastAccessTime+" Session:"+session.toString());
+			} else {
+				visitCount = (Integer)session.getAttribute(visitCountKey);
+				visitCount = visitCount + 1;
+				userID = (String)session.getAttribute(userIDKey);
+				log("OldSession//UIDKey:"+userIDKey+" UID:"+userID+" VisitCount:"+visitCount+" createTime:"+ createTime+ " lastAccess:"+lastAccessTime+" Session:"+session.toString());
+			}
+			session.setAttribute(visitCountKey,  visitCount);
 
-		      // Set response content type
-//		      response.setContentType("text/html");
-//		      PrintWriter out = response.getWriter();
+			response.getWriter().print("success");
+			response.addHeader("LOGIN_STATUS", "SUCCESS");
+			request.setAttribute("user", u);
+			response.sendRedirect("/TimeMachine/");
 
-//		      String docType =
-//		      "<!doctype html public \"-//w3c//dtd html 4.0 " +
-//		      "transitional//en\">\n";
-//		      out.println(docType +
-//		                "<html>\n" +
-//		                "<head><title>" + title + "</title></head>\n" +
-//		                "<body bgcolor=\"#f0f0f0\">\n" +
-//		                "<h1 align=\"center\">" + title + "</h1>\n" +
-//		                 "<h2 align=\"center\">Session Infomation</h2>\n" +
-//		                "<table border=\"1\" align=\"center\">\n" +
-//		                "<tr bgcolor=\"#949494\">\n" +
-//		                "  <th>Session info</th><th>value</th></tr>\n" +
-//		                "<tr>\n" +
-//		                "  <td>id</td>\n" +
-//		                "  <td>" + session.getId() + "</td></tr>\n" +
-//		                "<tr>\n" +
-//		                "  <td>Creation Time</td>\n" +
-//		                "  <td>" + createTime + 
-//		                "  </td></tr>\n" +
-//		                "<tr>\n" +
-//		                "  <td>Time of Last Access</td>\n" +
-//		                "  <td>" + lastAccessTime + 
-//		                "  </td></tr>\n" +
-//		                "<tr>\n" +
-//		                "  <td>User ID</td>\n" +
-//		                "  <td>" + userID + 
-//		                "  </td></tr>\n" +
-//		                "<tr>\n" +
-//		                "  <td>Number of visits</td>\n" +
-//		                "  <td>" + visitCount + "</td></tr>\n" +
-//		                "</table>\n" +
-//		                "</body></html>");
-		  
+			// Set response content type
+			//		      response.setContentType("text/html");
+			//		      PrintWriter out = response.getWriter();
+
+			//		      String docType =
+			//		      "<!doctype html public \"-//w3c//dtd html 4.0 " +
+			//		      "transitional//en\">\n";
+			//		      out.println(docType +
+			//		                "<html>\n" +
+			//		                "<head><title>" + title + "</title></head>\n" +
+			//		                "<body bgcolor=\"#f0f0f0\">\n" +
+			//		                "<h1 align=\"center\">" + title + "</h1>\n" +
+			//		                 "<h2 align=\"center\">Session Infomation</h2>\n" +
+			//		                "<table border=\"1\" align=\"center\">\n" +
+			//		                "<tr bgcolor=\"#949494\">\n" +
+			//		                "  <th>Session info</th><th>value</th></tr>\n" +
+			//		                "<tr>\n" +
+			//		                "  <td>id</td>\n" +
+			//		                "  <td>" + session.getId() + "</td></tr>\n" +
+			//		                "<tr>\n" +
+			//		                "  <td>Creation Time</td>\n" +
+			//		                "  <td>" + createTime + 
+			//		                "  </td></tr>\n" +
+			//		                "<tr>\n" +
+			//		                "  <td>Time of Last Access</td>\n" +
+			//		                "  <td>" + lastAccessTime + 
+			//		                "  </td></tr>\n" +
+			//		                "<tr>\n" +
+			//		                "  <td>User ID</td>\n" +
+			//		                "  <td>" + userID + 
+			//		                "  </td></tr>\n" +
+			//		                "<tr>\n" +
+			//		                "  <td>Number of visits</td>\n" +
+			//		                "  <td>" + visitCount + "</td></tr>\n" +
+			//		                "</table>\n" +
+			//		                "</body></html>");
+
 		} else {
+			response.getWriter().print("fail");
+			response.addHeader("LOGIN_STATUS", "FAILURE");
+			request.setAttribute("user", "");
 			response.sendRedirect("/TimeMachine/login/");
 		}
-		
+
 	}
 
 }
