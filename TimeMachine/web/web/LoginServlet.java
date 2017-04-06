9f7625c299b5a06b2689efb6196f7ab57f190d4b
erl67 <erl67@pitt.edu>
2017-04-05 21:17:12 -0400

    cookie for failed logins

--- TimeMachine/web/web/LoginServlet.java
+++ TimeMachine/web/web/LoginServlet.java
@@ -30,10 +30,10 @@ public class LoginServlet extends HttpServlet {
 		super();
 	}
 
-//	public static String LoginBean (String u, String p){
-//		System.out.println("Login Bean, u="+u+" p="+p);
-//		return bf.checkLogin(u, p);
-//	}
+	//	public static String LoginBean (String u, String p){
+	//		System.out.println("Login Bean, u="+u+" p="+p);
+	//		return bf.checkLogin(u, p);
+	//	}
 
 	public static String LoginBean2 (String u, String p){
 		System.out.println("Login Bean2:  u="+u+" p="+p);
@@ -53,22 +53,22 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		doGet(request, response);
 		log(request.toString()); log(response.toString());
-		
+
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		String u = LoginBean2(username, password);
-		
-	    final String cookieName = "TimeMachine_cookie";
-	    final String cookieValue = u;  // you could assign it some encoded value
-	    final Boolean useSecureCookie = false;
-	    final int expiryTime = 60 * 60 * 1;  // 1h in seconds
-	    final String cookiePath = "/";
-		
-	    response.setContentType("text/html");  
-	    PrintWriter out = response.getWriter();  
+
+		final String cookieName = "TimeMachine_cookie";
+		final String cookieValue = u;  // you could assign it some encoded value
+		final Boolean useSecureCookie = false;
+		final int expiryTime = 60 * 60 * 1;  // 1h in seconds
+		final String cookiePath = "/";
+
+		response.setContentType("text/html");  
+		PrintWriter out = response.getWriter();  
 
 		if (u != null) {
 			// Create a session object if it is already not  created.
@@ -96,23 +96,23 @@ public class LoginServlet extends HttpServlet {
 				log("OldSession//UIDKey:"+userIDKey+" UID:"+userID+" VisitCount:"+visitCount+" createTime:"+ createTime+ " lastAccess:"+lastAccessTime+" Session:"+session.toString());
 			}
 			session.setAttribute(visitCountKey,  visitCount);
-			
 
-		    Cookie tmc = new Cookie(cookieName, cookieValue);
-		    tmc.setSecure(useSecureCookie);  // determines whether the cookie should only be sent using a secure protocol, such as HTTPS or SSL
-		    tmc.setMaxAge(expiryTime);  // A negative value means that the cookie is not stored persistently and will be deleted when the Web browser exits. A zero value causes the cookie to be deleted.
-		    tmc.setPath(cookiePath);  // The cookie is visible to all the pages in the directory you specify, and all the pages in that directory's subdirectories
-		    response.addCookie(tmc);
-		    
-	        out.print("Login Success for user: " + u);  
-	        RequestDispatcher rd=request.getRequestDispatcher("index.html");  
-	        
+
+			Cookie tmc = new Cookie(cookieName, cookieValue);
+			tmc.setSecure(useSecureCookie);  // determines whether the cookie should only be sent using a secure protocol, such as HTTPS or SSL
+			tmc.setMaxAge(expiryTime);  // A negative value means that the cookie is not stored persistently and will be deleted when the Web browser exits. A zero value causes the cookie to be deleted.
+			tmc.setPath(cookiePath);  // The cookie is visible to all the pages in the directory you specify, and all the pages in that directory's subdirectories
+			response.addCookie(tmc);
+
+			out.print("Login Success for user: " + u);  
+			RequestDispatcher rd=request.getRequestDispatcher("index.html");  
+
 			response.getWriter().print("success");
 			response.addHeader("LOGIN_STATUS", "SUCCESS");
 			request.setAttribute("user", u);
 			response.sendRedirect("/TimeMachine/");
-			
-	        rd.include(request,response);  
+
+			rd.include(request,response);  
 
 			// Set response content type
 			//		      response.setContentType("text/html");
@@ -152,21 +152,21 @@ public class LoginServlet extends HttpServlet {
 			//		                "</body></html>");
 
 		} else {
-			
-		    Cookie tmc = new Cookie(cookieName, "Failed Login");
-		    tmc.setSecure(useSecureCookie);
-		    tmc.setMaxAge(expiryTime);	//set to 0 to delete cookie
-		    tmc.setPath(cookiePath);
-		    response.addCookie(tmc);
-			
-	        out.print("Sorry username or password error");  
-	        RequestDispatcher rd=request.getRequestDispatcher("index.html");  
-	        
+
+			Cookie tmc = new Cookie(cookieName, "Failed Login");
+			tmc.setSecure(useSecureCookie);
+			tmc.setMaxAge(expiryTime);	//set to 0 to delete cookie
+			tmc.setPath(cookiePath);
+			response.addCookie(tmc);
+
+			out.print("Sorry username or password error");  
+			RequestDispatcher rd=request.getRequestDispatcher("index.html");  
+
 			response.getWriter().print("fail");
 			response.addHeader("LOGIN_STATUS", "FAILURE");
 			request.setAttribute("user", "");
 			response.sendRedirect("/TimeMachine/login/");
-			
+
 			rd.include(request,response);  
 		}
 
